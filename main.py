96cb042548b4a5266157ca25d9b4ab36a31ff25c
Seshu Sai <105602908+iseshu@users.noreply.github.com>
2023-02-14 19:43:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,24 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 def home():
     return {'status':True}
 
+def stream_with_context(generator_function):
+  for chunk in generator_function:
+      yield chunk
+
 @app.route('/cors',methods=['GET','POST'])
 def coe():
     url = request.args.get('url')
     req = requests.get(url).content
     return req
+
+
+@app.route('/ibomma',methods=['GET','POST'])
+def download_file():
+    url = request.args.get('url')
+    resp = requests.get(url,headers = {"Referer" : "https://seucre-otp-ymflg-h002giy-ig-india.ibc.wf/"}, stream=True)
+    return Response(stream_with_context(resp.iter_content(chunk_size=1024)),
+                    content_type=resp.headers['Content-Type'])
+
 
 if __name__ == '__main__':
     app.run()
